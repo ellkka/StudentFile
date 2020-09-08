071f5c24072dca81b788501ca6535b1062ed2aa4
ellkka <mihaelatcacib@gmail.com>
2020-09-08 21:43:43 +0300

    Move from for loop to while

--- src/App.java
+++ src/App.java
@@ -4,23 +4,50 @@ public class App {
     public static void main(String[] args) {
 
         Scanner list = new Scanner(System.in);
-        System.out.println("Input number of students: ");
-        int size = Integer.parseInt(list.nextLine().trim());
-        System.out.println("Enter Name, age, status: ");
         Student stud = new Student();
         Student max = new Student();
 
-        for (int i = 0; i < size; i ++){
-            stud.setName(list.next());
-            stud.setAge(list.nextInt());
-            stud.setMarried(list.nextBoolean());
-            if (max.getAge() < stud.getAge() && stud.isMarried()){
-                max.setName(stud.getName());
-                max.setAge(stud.getAge());
+        while (true){
+            System.out.println("Enter a new user? (1: YES 0: NO): ");
+            int add = list.nextInt();
+
+            if (add == 1) {
+
+                System.out.println("Enter student name:");
+                stud.setName(list.next());
+                System.out.println("How old is the student?");
+                stud.setAge(list.nextInt());
+                System.out.println("Is student married?");
+                stud.setMarried(list.nextBoolean());
+
+
+            }
+            else if (add == 0) {
+                break;
+            }
+            else {
+                System.out.println("Please enter valid entry.");
             }
         }
-        System.out.println(max.getName() +"Is the oldest student not married. He is "+ max.getAge() + "old.");
-        list.close();
+
+//        Scanner list = new Scanner(System.in);
+//        System.out.println("Input number of students: ");
+//        int size = Integer.parseInt(list.nextLine().trim());
+//        System.out.println("Enter Name, age, status: ");
+//        Student stud = new Student();
+//        Student max = new Student();
+//
+//        for (int i = 0; i < size; i ++){
+//            stud.setName(list.next());
+//            stud.setAge(list.nextInt());
+//            stud.setMarried(list.nextBoolean());
+//            if (max.getAge() < stud.getAge() && stud.isMarried()){
+//                max.setName(stud.getName());
+//                max.setAge(stud.getAge());
+//            }
+//        }
+//        System.out.println(max.getName() +"Is the oldest student not married. He is "+ max.getAge() + "old.");
+//        list.close();
 
     }
 }
